d79de88ab1b3b0caedb7146b67f043e6efaf964e
Sashawangui <sasha.iluku@gmail.com>
2026-04-17 15:41:50 -0700

    Array created. Variables declared

---
+++ src/main/java/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    //variable declaration
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckout;
+    private String checkedOutTo;
+
+    public Book(int id, String title, String isbn){
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckout = false;
+        this.checkedOutTo = "";
+
+    }
+
+    //method to assign a checkout to an individual
+
+
+
+
+}
